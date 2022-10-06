acd6088dd80456ba95b2e513ca3168f2e6bb2271
Troy Michael Garidos <troymichael.garidos@biotechfarms.net>
2022-10-06 08:10:11 +0800

    upload add try-catch

--- src/route/upload/route.js
+++ src/route/upload/route.js
@@ -2,10 +2,14 @@ const { upload } = require('../../middleware/multer/upload-image')
 
 const userRouter = ({ router }) => {
   router.post('/', upload, (req, res) => {
-    if (req.files) {
-      res.status(201).send('images add succesfully')
-    } else if (req.files == undefined) {
-      res.status(400).send('Error: product_images is undefined')
+    try {
+      if (req.files) {
+        res.status(201).send('images add succesfully')
+      } else if (req.files == undefined) {
+        res.status(400).send('Error: product_images is undefined')
+      }
+    } catch (error) {
+      res.status(400).send('Error: ' + error)
     }
   })
   return router
